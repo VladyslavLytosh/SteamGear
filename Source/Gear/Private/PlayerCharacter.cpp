65a22a87f6d29b4b7ad51eaa4c2416e1b3c1a501
Vladyslav <vllitosh46@gmail.com>
2023-05-31 19:30:21 +0300

    Added Move and Look input function

--- Source/Gear/Private/PlayerCharacter.cpp
+++ Source/Gear/Private/PlayerCharacter.cpp
@@ -2,4 +2,81 @@
 
 
 #include "PlayerCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
 
+APlayerCharacter::APlayerCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	// We create camera from c++, because we need quick access to the camera component
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Player Camera"));
+	CameraComponent->SetupAttachment(GetRootComponent());
+	
+	// Config camera component setting, so we can make first person camera look
+	CameraComponent->bUsePawnControlRotation = true;
+	CameraComponent->SetRelativeLocation(FVector(-10.f,0.f,60.f));
+	
+}
+
+// Called when the game starts or when spawned
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void APlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+	// Add default Input Mapping Context
+	if (const TObjectPtr<APlayerController> PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if (const TObjectPtr<UEnhancedInputLocalPlayerSubsystem> Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext,0);
+		}
+	}
+}
+
+// Called to bind functionality to input
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	if (const TObjectPtr<UEnhancedInputComponent> EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction,ETriggerEvent::Triggered,this,&APlayerCharacter::Move);
+		EnhancedInputComponent->BindAction(LookAction,ETriggerEvent::Triggered,this,&APlayerCharacter::Look);
+		EnhancedInputComponent->BindAction(JumpAction,ETriggerEvent::Triggered,this,&APlayerCharacter::Jump);
+	}
+
+}
+
+void APlayerCharacter::Move(const FInputActionValue& Value)
+{
+	const FVector2D DirectionalValue = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		// We take Y axis from DirectionalValue, so we can move forward/backward
+		AddMovementInput(GetActorForwardVector(),DirectionalValue.Y);
+		// We take X axis from DirectionalValue, so we can move left/right
+		AddMovementInput(GetActorRightVector(),DirectionalValue.X);
+	}
+}
+
+void APlayerCharacter::Look(const FInputActionValue& Value)
+{
+	const FVector2D LookAxisValue = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		// We take X axis from DirectionalValue, so we can look left/right
+		AddControllerYawInput(LookAxisValue.X);
+		// We take Y axis from DirectionalValue, so we can look up/down
+		AddControllerPitchInput(LookAxisValue.Y);
+	}
+}
--- Source/Gear/Public/PlayerCharacter.h
+++ Source/Gear/Public/PlayerCharacter.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "BaseCharacter.h"
+#include "InputActionValue.h"
 #include "PlayerCharacter.generated.h"
 
 /**
@@ -13,5 +14,40 @@ UCLASS()
 class GEAR_API APlayerCharacter : public ABaseCharacter
 {
 	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APlayerCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<class UInputMappingContext> DefaultMappingContext;
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<class UInputAction> MoveAction;
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<class UInputAction> LookAction;
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<class UInputAction> JumpAction;
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<class UInputAction> SprintAction;
+
+	// We bind Move function to the corresponding input action, so character can move 
+	void Move(const FInputActionValue& Value);
+	// We bind Look function to the corresponding input action, so character can look around 
+	void Look(const FInputActionValue& Value);
+
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category = "Camera",meta = (AllowPrivateAccess = true))
+	TObjectPtr<class UCameraComponent> CameraComponent;
 	
 };
