c6c402386754c7d5a353414d577ed4b8e9aa01ba
Vladyslav <vllitosh46@gmail.com>
2023-05-31 18:46:49 +0300

    Added base classes for GameMode PlayerController and Character

---
+++ Source/Gear/Public/GearGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GearGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GEAR_API AGearGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
