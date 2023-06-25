0c481017a016d0187764ecde08ff2c1419cb3455
Vladyslav <vllitosh46@gmail.com>
2023-06-25 21:04:22 +0300

    Added State Machine plugin

---
+++ Plugins/StateMachine/Source/StateMachine/Private/StateBase.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StateMachine/Public/StateBase.h"
+
+void UStateBase::OnEnterState(TObjectPtr<AActor> StateOwner)
+{
+}
+
+void UStateBase::TickState()
+{
+}
+
+void UStateBase::OnExitState()
+{
+}
--- Source/Gear/Public/Characters/PlayerCharacter.h
+++ Source/Gear/Public/Characters/PlayerCharacter.h
@@ -41,7 +41,7 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 
 private:
 	// Pointers to various input actions
